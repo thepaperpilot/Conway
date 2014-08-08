2b3eb9b11013b37794df358262a898aa5930b03b
The Paper Pilot <thepaperpilot@gmail.com>
2014-08-07 22:44:07 -0500

    Added temporary skin Added utility class ConwayScreen Added title screen Added an asset manager

--- core/src/com/thepaperpilot/Conway.java
+++ core/src/com/thepaperpilot/Conway.java
@@ -1,27 +1,26 @@
 package com.thepaperpilot;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.thepaperpilot.screens.Title;
 
-public class Conway extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+public class Conway extends Game {
+	public static AssetManager manager = new AssetManager();
+
+	public static Skin skin;
+
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture(Gdx.files.internal("badlogic.jpg"));
+		setScreen(new Title(this));
 	}
 
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+	public void render() {
+		Gdx.gl.glClearColor(0, 0, 0, 0);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		super.render();
 	}
 }
